769f3bfb0b6f3d96b77034ee8df8da805b3bfb41
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-03 13:44:09 +0000

    fix detail

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,8 +8,9 @@ class BookCommentsController < ApplicationController
     end
     
     def destroy
-        BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-        redirect_to book_path(params[:book_id])
+        @book = Book.find(params[:book_id])
+        @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+        @book_comment.destroy
     end
 
     private
